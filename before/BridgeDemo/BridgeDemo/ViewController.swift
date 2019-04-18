68e75fab5a0a5b7a8686fa7cc4ea911c3d2b27db
WenslowZhu <wenslow.vip@gmail.com>
2019-04-18 10:57:21 +0800

    init Bridge Demo

---
+++ before/BridgeDemo/BridgeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BridgeDemo
+//
+//  Created by tstone10 on 2019/4/18.
+//  Copyright © 2019 wenslow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
